Normalize images in sift_matching function and fix determinant calculation in Harris corner detection

sift/descriptor.py
@@ -222,18 +222,11 @@ def normalize_sift_feature(hists: np.ndarray) -> np.ndarray:
 
 def assign_descriptor(keypoints: list[Keypoint],
                       gauss_octave: np.array,
-                      octave_idx: int) -> list[Keypoint]:
+                      octave_idx: int) :
     """ Assigns a descriptor to each keypoint.
         A descriptor is a collection of histograms that capture
         the distribution of gradients orientations in an oriented
-        keypoint's local neighborhood. See AOS section 4.2 and Lowe
-        section 6. Descriptors are created by taking a square
-        patch of gradients surrounding the keypoint, assigning
-        each gradient in the patch a coordinates relative to the
-        oriented keypoint, and accumulating the gradients into a set
-        of histograms. A gradient's contributions to a particular
-        histogram is determined by its distance from the histogram's
-        and keypoint's location.
+        keypoint's local neighborhood.
 
     Args:
         keypoints: A list of keypoints that have been assigned an orientation.

sift/keypoints.py
@@ -32,14 +32,6 @@ class Keypoint:
 
 
 def in_frame(coord: np.ndarray, shape: np.ndarray) -> bool:
-    """ Determines whether a coordinate falls within an array.
-
-    Args:
-        coord: A coordinate.
-        shape: The shape of the an array.
-    Returns :
-        True if in frame, False if not.
-    """
     return (coord >= 0).all() and (coord <= shape - 1).all()
 
 
@@ -84,13 +76,8 @@ def  interpolate(extremum_coord: np.ndarray,
                 dog_octave: np.ndarray,
                 derivs: np.ndarray,
                 second_derivs: np.ndarray) -> Tuple[bool, np.ndarray, float]:
-    """ Interpolates the coordinate and value of an extremum in
-        a Difference of Gaussian octave. Interpolation is performed
-        by a Taylor expansion to fit a 3D quadratic function to the
-        local sample. The interpolated extremum is where this function's
-        derivative equals 0. This enables more precise sub-pixel keypoint
-        locations, which improves descriptor quality. For details,
-        see Lowe section 4 and AOS section 3.2.
+    """ Interpolation is performed by a Taylor expansion to fit a 3D quadratic function to the
+        local sample.
 
     Args:
         extremum_coord: Non-interpolated coordinates of an extremum.
@@ -129,17 +116,14 @@ def  interpolate(extremum_coord: np.ndarray,
 
 def find_keypoints(extrema: np.ndarray, dog_octave: np.ndarray) -> np.ndarray:
     """ Finds valid keypoint coordinates among candidate Difference of
-        Gaussian extrema. A candidate keypoint coordinate must be
-        interpolated, pass a magnitude threshold, and pass the
-        edge test.
-
+        Gaussian extrema. 
     Args:
         extrema: Difference of Gaussian extrema coordinates.
         dog_octave: An octave of Difference of Gaussian images.
     Returns:
         keypoint_coords: Interpolated coordinates of potential keypoints.
     """
-    keypoint_coords = list()
+    keypoint_coords = []
     derivs, second_derivs = derivatives(dog_octave)
 
     for extremum_coord in extrema.T:

sift/octaves.py
@@ -12,8 +12,7 @@ from . import const
 
 def relative_sigma(layer_idx: int):
     """
-        Section 2.2 Digital Gaussian Space
-        layer_sigma = (octave_idx / min_pixel_dist) * min_sigma * 2 ** (layer_idx / scales_per_octave)
+    layer_sigma = (octave_idx / min_pixel_dist) * min_sigma * 2 ** (layer_idx / scales_per_octave)
 
     Args:
         layer_idx: The index of a layer in octave.
@@ -28,9 +27,8 @@ def relative_sigma(layer_idx: int):
 
 def absolute_sigma(octave_idx: int,
                    layer_idx: int):
-    """ Calculates layer's absolute sigma: the level of blurring required to move from the original image
-        to this layer in scale-space.
-
+    """
+        Calculates layer's absolute sigma
     Args:
         octave_idx: Note we start with the octave of the base image
     Returns:
@@ -55,7 +53,7 @@ def build_gaussian_octaves(img: np.ndarray):
 
     for octave_idx in range(const.nr_octaves):
 
-        #first octave 2x upsampled input image.
+        #first octave 2x upsampled input image... Base Image
         if octave_idx == 0:
             img = cv2.resize(img, None, fx=const.first_upscale, fy=const.first_upscale, interpolation=cv2.INTER_LINEAR)
             img = gaussian_filter(img, const.init_sigma)
@@ -79,7 +77,7 @@ def build_gaussian_octaves(img: np.ndarray):
     return octaves
 
 
-def build_dog_octave(gauss_octave: np.ndarray) -> np.ndarray:
+def build_dog_octave(gauss_octave: np.ndarray):
     """ Builds a Difference of Gaussian octave.
 
     Args:
@@ -120,12 +118,9 @@ def shift(array: np.ndarray,
     return shifted
 
 
-def find_dog_extrema(dog_octave: np.ndarray) -> np.ndarray:
+def find_dog_extrema(dog_octave: np.ndarray):
     """ Finds extrema in a Difference of Gaussian octave.
-        This is achieved by subtracting a cell by all it's
-        direct (including diagonal) neighbors, and confirming
-        all differences have the same sign.
-
+    
     Args:
         dog_octave: An octave of Difference of Gaussian images.
     Returns:
@@ -134,6 +129,11 @@ def find_dog_extrema(dog_octave: np.ndarray) -> np.ndarray:
     shifts = list(itertools.product([-1, 0, 1], repeat=3))
     shifts.remove((0, 0, 0))
 
+    # 0 0
+    # 0 1 
+    # 1 2 3  7 8 9
+    # 4 5 6  1 2 3 
+    # 7 8 9  4 5 6
     diffs = []
     for shift_spec in shifts:
         shifted = shift(dog_octave, shift_spec)
@@ -148,17 +148,8 @@ def find_dog_extrema(dog_octave: np.ndarray) -> np.ndarray:
     return extrema_coords
 
 
-def derivatives(dog_octave: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    """ Calculates the first and second order s, y, x derivatives for an octave.
-
-    Args:
-        dog_octave: An octave of Difference of Gaussian images.
-    Returns:
-        derivs: The s, y, and x derivatives of the octave.
-        second_derivs: The ss, yy, xx, sy, sx, yx derivatives of the octave.
-            Provided in the format of a flattened Hessian for convenient
-            indexing and reshaping.
-    """
+def derivatives(dog_octave: np.ndarray):
+   
     o = dog_octave
 
     ds = (shift(o, [1, 0, 0]) - shift(o, [-1, 0, 0])) / 2

sift/reference_orientation.py
@@ -138,16 +138,10 @@ def weighting_matrix(center_offset: np.array,
     return weights
 
 
-def find_histogram_peaks(hist: np.array) -> List[float]:
+def find_histogram_peaks(hist: np.array) :
     """ Finds peaks in the gradient orientations histogram,
         and returns the corresponding orientations in radians.
-        Peaks are the maximum bin and bins that lie within 0.80
-        of the mass of the maximum bin. See AOS section 4.1 and
-        Lowe section 5. When the modulo operator is used in this
-        function, it is to  account for the fact that the first
-        and last bin are neighbors, namely, the rotations by 0
-        and 2pi radians.
-
+    
     Args:
         hist: Histogram where each bin represents an orientation, in other
             words, an angle of a gradient. The mass of the bin is determined
@@ -191,13 +185,8 @@ def find_histogram_peaks(hist: np.array) -> List[float]:
 
 def assign_reference_orientations(keypoint_coords: np.array,
                                   gauss_octave: np.array,
-                                  octave_idx: int) -> list[Keypoint]:
+                                  octave_idx: int):
     """ Assigns dominant local neighborhood gradient orientations to keypoints.
-        These dominant orientations are also known as reference orientations.
-        A keypoint coordinate may have multiple reference orientations.
-        In that case, multiple Keypoint objects are created for that coordinate.
-        Reference orientations are used to create rotation invariant descriptors.
-        See Lowe section 5, AOS section 4.1.
 
     Args:
         keypoint_coords: The keypoints' 3D coordinates.

sift_matching.py
@@ -11,7 +11,7 @@ from sift.octaves import build_gaussian_octaves, build_dog_octave, find_dog_extr
 from sift.reference_orientation import assign_reference_orientations
 
 
-def detect_sift_features(img: np.ndarray) -> list[Keypoint]:
+def detect_sift_features(img: np.ndarray):
     """ Detects SIFT keypoints in an image.
 
     Args: